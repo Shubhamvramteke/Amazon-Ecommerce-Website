6255cefc7ff23cc9805a965360f7243bf31b679f
Shubham Ramteke <shubham@EvolveWare.com>
2025-02-17 14:28:13 +0530

    Product catalog project commit

---
+++ product-catlog-service/src/test/java/org/example/productcatlogservice/ProductCatlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productcatlogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
